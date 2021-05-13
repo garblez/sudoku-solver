55214a3d8d2bc1f3dd75a9c851cdfd9aaa2de758
Max Kolle <maxkolle1@gmail.com>
2021-05-13 20:59:58 +0100

    Add Status enum

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,17 @@ enum Value {
     Fail // A search has failed: inform the solver to quit
 }
 
+
+enum Status {
+    Solved, Failed
+}
+
+
+struct Board {
+    cells: Vec<Value>,
+    solved: Status
+}
+
 fn print_board(board: &Vec<Value>) {
     println!("Size of board init: {}", board.len());
     for row in 0..9 {
@@ -64,14 +75,37 @@ impl Value {
 }
 
 // Check if a given grid cell is unique in a row
-fn unique_in_row(board: &Vec<Value>, row: usize, var: Value) -> bool {
-    let mut occurence = 0;
-    for col in row..row+9 {
+fn unique_in_row(board: &Vec<Value>, var: &Value, row: usize) -> bool {
+    let mut occurrence = 0;
+    for col in 0..9 {
         if board[row + col*9+1].get_value() == var.get_value() {
-            occurence += 1;
+            occurrence += 1;
         }
     }
-    occurence == 1
+    occurrence == 1
+}
+
+fn unique_in_col(board: &Vec<Value>, var: &Value, col: usize) -> bool {
+    let mut occurrence = 0;
+    for row in 0..9 {
+        if board[row + col*9 + 1].get_value() == var.get_value() {
+            occurrence += 1;
+        }
+    }
+    occurrence == 1
+}
+
+fn unique_in_square(board: &Vec<Value>, var: &Value, center: (usize, usize)) -> bool {
+    let mut occurrence = 0;
+    let (row, col) = center;
+    for r in row-1..=row+1 {
+        for c in col-1..=col+1 {
+            if board[r + c*9 + 1].get_value() == var.get_value() {
+                occurrence += 1;
+            }
+        }
+    }
+    occurrence == 1
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<fs::File>>>
@@ -79,3 +113,9 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<fs::File>>>
         let file = fs::File::open(filename).expect("Unable to open file!");
         Ok(io::BufReader::new(file).lines())
     }
+
+
+fn proceed(board: &mut Vec<Value>, i: usize) -> usize {
+    let mut consistent = false;
+    0  // TODO: REMOVE
+}
